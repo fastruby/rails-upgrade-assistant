23b178cc1212497f28d909742c3187a1d174005a
Ariel Juodziukynas <arieljuod@gmail.com>
2023-02-13 16:47:33 -0300

    Improve select patch and add ref

--- lib/rails_upgrade_assistant/patchers/rails71.rb
+++ lib/rails_upgrade_assistant/patchers/rails71.rb
@@ -10,7 +10,7 @@ class Rails71 < Patcher
       alias_method :original_select, :select
 
       def select(method, choices = nil, options = {}, html_options = {}, &block)
-        if choices && options[:required] && !options[:include_blank]
+        if choices && options[:required] && !options[:include_blank] && !options[:prompt]
           choices_has_blank =
             if choices.is_a?(String)
               choices.match?(/value="\s*"/)
@@ -19,7 +19,7 @@ class Rails71 < Patcher
             end
 
           unless choices_has_blank
-            Rails71.warn("f.select now adds an empty option when `required: true` is provided, and current options do not include an empty one")
+            Rails71.warn("f.select now adds an empty option tag when `required: true` is provided in the `options` hash, and current option tags do not include an empty one (ref: https://github.com/rails/rails/commit/49c2e51808aaa89f3a0e833b6c42d97732f7d039)")
           end
         end
         original_select(method, choices, options, html_options, &block)
